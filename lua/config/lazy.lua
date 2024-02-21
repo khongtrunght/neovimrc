4fec783f96a253a03ff5c7f0e440d4ee811394bf
khongtrunght <khongtrunght@gmail.com>
2024-02-21 10:57:48 +0700

    first setting

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -6,18 +6,28 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
---require("lazyvim.config").init()
 require("lazy").setup({
   spec = {
-    { import = "lazyvim.plugins" },
+    -- add LazyVim and import its plugins
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- import any extras modules here
+    -- { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- { import = "lazyvim.plugins.extras.lang.json" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- import/override with your plugins
     { import = "plugins" },
   },
   defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
-    version = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  checker = { enabled = true },
-  install = { colorscheme = { "tokyonight", "habamax", "catppuccin" } },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
@@ -34,43 +44,3 @@ require("lazy").setup({
     },
   },
 })
-require("lazyvim.config").setup({})
---
--- require("lazy").setup({
---   spec = {
---     -- add LazyVim and import its plugins
---     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
---     -- import any extras modules here
---     -- { import = "lazyvim.plugins.extras.lang.typescript" },
---     -- { import = "lazyvim.plugins.extras.lang.json" },
---     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
---     -- import/override with your plugins
---     { import = "plugins" },
---   },
---   defaults = {
---     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
---     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
---     lazy = false,
---     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
---     -- have outdated releases, which may break your Neovim install.
---     version = false, -- always use the latest git commit
---     -- version = "*", -- try installing the latest stable version for plugins that support semver
---   },
---   install = { colorscheme = { "tokyonight", "habamax" } },
---   checker = { enabled = true }, -- automatically check for plugin updates
---   performance = {
---     rtp = {
---       -- disable some rtp plugins
---       disabled_plugins = {
---         "gzip",
---         -- "matchit",
---         -- "matchparen",
---         -- "netrwPlugin",
---         "tarPlugin",
---         "tohtml",
---         "tutor",
---         "zipPlugin",
---       },
---     },
---   },
--- })
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,17 +1,27 @@
 return {
-  { "ellisonleao/gruvbox.nvim", config = true, opts = ... },
+  -- { "ellisonleao/gruvbox.nvim", config = true, opts = ... },
 
   {
     "projekt0n/github-nvim-theme",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require("github-theme").setup({
-        -- ...
-      })
+    -- lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    -- priority = 1000, -- make sure to load this before all the other start plugins
+    -- config = function()
+    --   require("github-theme").setup({
+    --     -- ...
+    --   })
+    --
+    --   vim.cmd("colorscheme github_dark")
+    -- end,
+  },
 
-      vim.cmd("colorscheme github_dark")
-    end,
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
   },
 
   {
