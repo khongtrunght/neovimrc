281b166458edda1321484a0e6d8e5c77658cf029
khongtrunght <khongtrunght@gmail.com>
2024-02-25 17:46:01 +0700

    fix clangd error
    clangd error "multiple different client offset_encoding detect for this buffer, this is not support yet"

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -12,3 +12,12 @@ require("neo-tree").setup({
     },
   },
 })
+
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
+require("lspconfig").clangd.setup({
+  capabilities = cmp_nvim_lsp.default_capabilities(),
+  cmd = {
+    "clangd",
+    "--offset-encoding=utf-16",
+  },
+})
