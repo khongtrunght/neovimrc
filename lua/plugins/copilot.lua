dcd712a82c588ea3a56ca0bcaeb21fbbe05d1c04
khongtrunght <khongtrunght@gmail.com>
2024-02-25 22:26:33 +0700

    disable copilot c, c++

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,11 @@
+return {
+  "zbirenbaum/copilot.lua",
+  opts = {
+    filetypes = {
+      markdown = true,
+      help = true,
+      c = false, -- Disable Copilot for C files
+      cpp = false, -- Disable Copilot for C++ files
+    },
+  },
+}
