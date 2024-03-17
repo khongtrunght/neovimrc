26c83b7423274764217ff9568198457fd17aa899
khongtrunght <khongtrunght@gmail.com>
2024-03-17 13:07:03 +0700

    change harpoon to grapple

---
+++ lua/plugins/grapple.lua
@@ -0,0 +1,37 @@
+return {
+  "cbochs/grapple.nvim",
+  dependencies = {
+    { "nvim-tree/nvim-web-devicons", lazy = true },
+  },
+  opts = {
+    -- scope = "git", -- also try out "git_branch"
+    icons = true,
+    status = true,
+  },
+  event = { "BufReadPost", "BufNewFile" },
+  cmd = "Grapple",
+  keys = {
+    { "<leader>a", "<cmd>Grapple toggle<cr>", desc = "Tag a file" },
+    { "<leader>ts", "<cmd>Grapple toggle_scopes<cr>", desc = "Grapple toggle scopes" },
+    { "<leader>h", "<cmd>Telescope grapple tags<cr>", desc = "Open grapple windows with telescope" },
+    { "<c-e>", "<cmd>Grapple toggle_tags<cr>", desc = "Toggle tags menu" },
+    { "<c-k>", "<cmd>Grapple cycle forward<cr>", desc = "Grapple cycle forward" },
+    { "<c-j>", "<cmd>Grapple cycle backward<cr>", desc = "Grapple cycle backward" },
+    { "<leader>1", "<cmd>Grapple select index=1<cr>", desc = "Grapple select 1" },
+    { "<leader>2", "<cmd>Grapple select index=2<cr>", desc = "Grapple select 2" },
+    { "<leader>3", "<cmd>Grapple select index=3<cr>", desc = "Grapple select 3" },
+    { "<leader>4", "<cmd>Grapple select index=4<cr>", desc = "Grapple select 4" },
+    -- { "C-j>", "<cmd>Grapple select index=1<cr>", desc = "Select tag 1" },
+    -- { "C-k>", "<cmd>Grapple select index=2<cr>", desc = "Select tag 2" },
+    -- { "C-l>", "<cmd>Grapple select index=3<cr>", desc = "Select tag 3" },
+    -- { "C-;>", "<cmd>Grapple select index=4<cr>", desc = "Select tag 4" },
+  },
+  config = function()
+    require("telescope").load_extension("grapple")
+    require("lualine").setup({
+      sections = {
+        lualine_b = { "grapple" },
+      },
+    })
+  end,
+}
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,91 +1,92 @@
-return {
-  "theprimeagen/harpoon",
-  branch = "harpoon2",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {
-    global_settings = {
-      save_on_toggle = false,
-      save_on_change = true,
-    },
-  },
-  keys = {
-    {
-      "<leader>a",
-      function()
-        require("harpoon"):list():append()
-      end,
-      desc = "harpoon file",
-    },
-    --{ "<leader>h", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "harpoon quick menu", },
-    {
-      "<leader>1",
-      function()
-        require("harpoon"):list():select(1)
-      end,
-      desc = "Harpoon to file 1",
-    },
-    {
-      "<leader>2",
-      function()
-        require("harpoon"):list():select(2)
-      end,
-      desc = "Harpoon to file 2",
-    },
-    {
-      "<leader>3",
-      function()
-        require("harpoon"):list():select(3)
-      end,
-      desc = "Harpoon to file 3",
-    },
-    {
-      "<leader>4",
-      function()
-        require("harpoon"):list():select(2)
-      end,
-      desc = "Harpoon to file 4",
-    },
-    {
-      "<leader>5",
-      function()
-        require("harpoon"):list():prev()
-      end,
-      desc = "Prev file",
-    },
-    {
-      "<leader>6",
-      function()
-        require("harpoon"):list():next()
-      end,
-      desc = "Next file",
-    },
-    {
-      "<C-j>",
-      function()
-        require("harpoon"):list():select(1)
-      end,
-      desc = "Harpoon to file 1",
-    },
-    {
-      "<C-k>",
-      function()
-        require("harpoon"):list():select(2)
-      end,
-      desc = "Harpoon to file 1",
-    },
-    {
-      "<C-l>",
-      function()
-        require("harpoon"):list():select(3)
-      end,
-      desc = "Harpoon to file 1",
-    },
-    {
-      "<C-;>",
-      function()
-        require("harpoon"):list():select(4)
-      end,
-      desc = "Harpoon to file 1",
-    },
-  },
-}
+return {}
+-- return {
+--   "theprimeagen/harpoon",
+--   branch = "harpoon2",
+--   dependencies = { "nvim-lua/plenary.nvim" },
+--   opts = {
+--     global_settings = {
+--       save_on_toggle = false,
+--       save_on_change = true,
+--     },
+--   },
+--   keys = {
+--     {
+--       "<leader>a",
+--       function()
+--         require("harpoon"):list():append()
+--       end,
+--       desc = "harpoon file",
+--     },
+--     --{ "<leader>h", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "harpoon quick menu", },
+--     {
+--       "<leader>1",
+--       function()
+--         require("harpoon"):list():select(1)
+--       end,
+--       desc = "Harpoon to file 1",
+--     },
+--     {
+--       "<leader>2",
+--       function()
+--         require("harpoon"):list():select(2)
+--       end,
+--       desc = "Harpoon to file 2",
+--     },
+--     {
+--       "<leader>3",
+--       function()
+--         require("harpoon"):list():select(3)
+--       end,
+--       desc = "Harpoon to file 3",
+--     },
+--     {
+--       "<leader>4",
+--       function()
+--         require("harpoon"):list():select(2)
+--       end,
+--       desc = "Harpoon to file 4",
+--     },
+--     {
+--       "<leader>5",
+--       function()
+--         require("harpoon"):list():prev()
+--       end,
+--       desc = "Prev file",
+--     },
+--     {
+--       "<leader>6",
+--       function()
+--         require("harpoon"):list():next()
+--       end,
+--       desc = "Next file",
+--     },
+--     {
+--       "<C-j>",
+--       function()
+--         require("harpoon"):list():select(1)
+--       end,
+--       desc = "Harpoon to file 1",
+--     },
+--     {
+--       "<C-k>",
+--       function()
+--         require("harpoon"):list():select(2)
+--       end,
+--       desc = "Harpoon to file 1",
+--     },
+--     {
+--       "<C-l>",
+--       function()
+--         require("harpoon"):list():select(3)
+--       end,
+--       desc = "Harpoon to file 1",
+--     },
+--     {
+--       "<C-;>",
+--       function()
+--         require("harpoon"):list():select(4)
+--       end,
+--       desc = "Harpoon to file 1",
+--     },
+--   },
+-- }
