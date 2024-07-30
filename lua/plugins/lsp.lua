0c445de9c50b9aee48eb1829e7d273783da42bc1
khongtrunght <khongtrunght@gmail.com>
2024-07-30 15:04:20 +0700

    add templ

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,6 +2,27 @@ local Util = require("lazyvim.util")
 local augroup = vim.api.nvim_create_augroup
 local ThePrimeagenGroup = augroup("ThePrimeagen", {})
 local autocmd = vim.api.nvim_create_autocmd
+
+-- local custom_format = function()
+--   if vim.bo.filetype == "templ" then
+--     local bufnr = vim.api.nvim_get_current_buf()
+--     local filename = vim.api.nvim_buf_get_name(bufnr)
+--     local cmd = "templ fmt " .. vim.fn.shellescape(filename)
+--
+--     vim.fn.jobstart(cmd, {
+--       on_exit = function()
+--         -- Reload the buffer only if it's still the current buffer
+--         if vim.api.nvim_get_current_buf() == bufnr then
+--           vim.cmd("e!")
+--         end
+--       end,
+--     })
+--   else
+--     vim.lsp.buf.format()
+--   end
+-- end
+--
+-- vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = { "*.templ" }, callback = custom_format })
 autocmd("LspAttach", {
   group = ThePrimeagenGroup,
   callback = function(e)
